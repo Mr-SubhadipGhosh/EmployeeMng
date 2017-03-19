4a5f2c014d8e57624581420d55ecf119aeaec8fa
Dell <SUBHADIP@Dell>
2017-03-19 13:19:17 +0530

    project added

---
+++ EmployeeMgr/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EmployeeMgr.Startup))]
+namespace EmployeeMgr
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
