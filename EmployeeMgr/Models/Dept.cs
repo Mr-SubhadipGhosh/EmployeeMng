98e120c9190ed1be613d9826c0440e6168a4795c
Dell <SUBHADIP@Dell>
2017-03-26 13:45:29 +0530

    added emp

---
+++ EmployeeMgr/Models/Dept.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EmployeeMgr.Models
+{
+    public class Dept
+    {
+        public int deptId { get; set; }
+        public string deptName { get; set; }
+    }
+}
---
+++ EmployeeMgr/Models/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace EmployeeMgr.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int empId { get; set; }
+        public string empName { get; set; }
+        [ForeignKey("Dept")]
+        public int deptId { get; set; }
+
+        public virtual Dept deptOfEmp { get; set; }
+    }
+}
